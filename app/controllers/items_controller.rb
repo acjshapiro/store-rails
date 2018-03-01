63cd27ecc08e6bc3428ca5d757e9b0497efcf455
Alex Shapiro <acjshapiro@gmail.com>
2018-02-28 19:51:58 -0700

    Forms/views/itemcontroller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,56 @@
 class ItemsController < ApplicationController
+  before_action :set_dept
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
+    @items = @dept.items
   end
 
   def show
   end
 
-  def edit
+  def new
+    @item = @dept.items.new
+    render partial: 'form'
   end
 
-  def new
+  def edit
+    render partial: 'form'
   end
+
+  def creat
+    @item = @dept.items.new(item_params)
+    if @item.save
+      redirect_to[@dept, @item]
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to [@dept, @item]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to dept_items_path
+  end
+
+  private
+    def set_dept
+      @dept = Dept.find(params[:dept_id])
+    end
+
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def item_params
+      params.require(:item).permit(:name, :description, :price)
+    end   
+
 end
